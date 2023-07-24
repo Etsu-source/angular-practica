35a0013edf239b0f86acd3c036cf7d9d8d843795
unknown <joserock7395@gmail.com>
2023-07-23 20:11:27 -0500

    practica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'animes', loadChildren: () => import('./animes/animes.module').then(m => m.AnimesModule)
+  },
+
+  {
+    path: '404',
+    component: ErrorPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
